9c818b4cf679d45d294d53dd31da00fb5b8b6f7e
RadAesc <michaelbuslik@hotmail.com>
2018-07-02 20:17:12 -0700

    Better control and added platforms.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,28 +7,73 @@ public class PlayerMove
     :
     MonoBehaviour
 {
-    const float speed = 20.0f;
-    Rigidbody2D body;
     // Use this for initialization
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        Assert.IsTrue( body != null );
+        Assert.IsNotNull( body );
 
-        if( Input.GetAxis( "Move" ) < 0.0f )
+        float dt = GetDT();
+
+        if( Input.GetAxis( "Move Left" ) > 0.0f )
         {
-            body.AddForce( new Vector2( -speed,0.0f ) );
+            body.AddForce( new Vector2( -speed *
+                dt,0.0f ) );
         }
-        if( Input.GetAxis( "Move" ) > 0.0f )
+        if( Input.GetAxis( "Move Right" ) > 0.0f )
         {
-            body.AddForce( new Vector2( speed,0.0f ) );
+            body.AddForce( new Vector2( speed *
+                dt,0.0f ) );
         }
 
-        body.AddForce( -body.velocity * 3.0f );
+        body.AddForce( -body.velocity * 3.0f * dt );
+
+        if( Input.GetAxis( "Jump" ) > 0.0f )
+        {
+            if( canJump )
+            {
+                jumping = true;
+                canJump = false;
+            }
+        }
+        else if( minJump.IsDone() ) FinishJump();
+
+        if( jumping )
+        {
+            body.AddForce( new Vector2( 0.0f,jumpPower *
+                dt * ( ( 1.0f - maxJump.GetPercent() ) * 1.5f ) ) );
+
+            maxJump.Update( Time.deltaTime );
+            minJump.Update( Time.deltaTime );
+
+            if( maxJump.IsDone() ) FinishJump();
+        }
     }
+    void FinishJump()
+    {
+        maxJump.Reset();
+        minJump.Reset();
+        jumping = false;
+    }
+    void OnCollisionEnter2D( Collision2D coll )
+    {
+        canJump = true;
+    }
+    float GetDT()
+    {
+        return ( Time.deltaTime * dtOffset );
+    }
+    // 
+    const float dtOffset = 1.0f / 0.01700295f;
+    const float speed = 20.0f;
+    Rigidbody2D body;
+    const float jumpPower = 45.4f;
+    bool jumping = false;
+    bool canJump = false;
+    Timer minJump = new Timer( 0.11f );
+    Timer maxJump = new Timer( 0.75f );
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer
+{
+    public Timer( float max )
+    {
+        this.max = max;
+    }
+    public void Update( float dt )
+    {
+        if( cur <= max ) cur += dt;
+    }
+    public void Reset()
+    {
+        cur = 0.0f;
+    }
+    public bool IsDone()
+    {
+        return( cur >= max );
+    }
+    public float GetPercent()
+    {
+        return( cur / max );
+    }
+    // 
+    float max;
+    float cur = 0.0f;
+}
