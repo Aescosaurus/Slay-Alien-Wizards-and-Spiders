ea69e436e23e116bfc2c20918d794d17cdabe937
RadAesc <michaelbuslik@hotmail.com>
2018-07-08 17:07:29 -0700

    Fixed the heckin aspect ratio BS.

---
+++ Assets/Scripts/FixCameraAspect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixCameraAspect
+    :
+    MonoBehaviour
+{
+    void Start()
+    {
+        const float width = 16.0f;
+        const float height = 9.0f;
+
+        Camera.main.aspect = width / height;
+        // Camera.main.ResetAspect();
+    }
+}
